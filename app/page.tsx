5856f4e0f0f9d824abd955a09439095016ab8491
arieldinnisoft <ariel@dinnisoft.com.br>
2025-07-19 17:00:10 -0300

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 import Agents from "./components/agents/page";
 import AskchatCRM from "./components/askchatCRM/page";
 import Brands from "./components/brands/page";
-import Footer from "./components/footer/page";
-import Form from "./components/form/page";
 import Header from "./components/header/page";
-import Solutions from "./components/solutions/page";
 import Platform from "./components/platform/page";
-import Customers from "./components/customers/page";
+// import Footer from "./components/footer/page";
+// import Form from "./components/form/page";
+// import Solutions from "./components/solutions/page";
+// import Customers from "./components/customers/page";
 
 export default function Home() {
   return (
